1d392cbcd11eb56944f3c376c6b8a8fd4fbd2244
bcook03 <bnc121203@outlook.com>
2025-02-13 19:31:00 -0600

    updated Slingshot

--- Assets/Script/Slingshot.cs
+++ Assets/Script/Slingshot.cs
@@ -4,13 +4,21 @@ using UnityEngine;
 
 public class Slingshot : MonoBehaviour
 {
+    [Header("Inscribed")]
+    public GameObject projectilePrefab;
+
+    [Header("Dynamic")]
     public GameObject launchPoint;
+    public Vector3 launchPos;
+    public GameObject projectile;
+    public bool aimingMode;
 
     void Awake()
     {
         Transform launchPointTrans = transform.Find("LaunchPoint");
         launchPoint = launchPointTrans.gameObject;
         launchPoint.SetActive( false );
+        launchPos = launchPointTrans.position;
     }
 
     void OnMouseEnter(){
@@ -19,6 +27,11 @@ public class Slingshot : MonoBehaviour
 
     void OnMouseExit(){
         launchPoint.SetActive(false);
+    }
+
+    void OnMouseDown()
+    {
+           
     }
 } 
 

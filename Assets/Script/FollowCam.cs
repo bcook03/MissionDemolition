1c0e58a198032bbefe3874511753c9373abcf937
bcook03 <bnc121203@outlook.com>
2025-02-13 20:37:00 -0600

    updated FollowCam

--- Assets/Script/FollowCam.cs
+++ Assets/Script/FollowCam.cs
@@ -6,6 +6,9 @@ public class FollowCam : MonoBehaviour
 {
     static public GameObject POI; // The static point of interest
 
+    [Header("Inscribed")]
+    public float easing = 0.05f;
+    public Vector2 minXY = Vector2.zero;
     [Header("Dynamic")]
     public float camZ; // The desired Z pos of the camera
 
@@ -16,10 +19,26 @@ public class FollowCam : MonoBehaviour
 
     void FixedUpdate()
     {
-        if (POI == null) return;
+        Vector3 destination = Vector3.zero;
 
-        // Get the position of the POI
-        Vector3 destination = POI.transform.position;
+        if (POI != null) {
+            // If the POI has a Rigidbody, check to see if it is sleeping
+            Rigidbody poiRigid = POI.GetComponent<Rigidbody>();
+            if ( (poiRigid != null) && poiRigid.IsSleeping()) {
+                POI = null;
+            }
+
+        }
+
+        if (POI != null) {
+            destination = POI.transform.position;
+        }
+        
+        // Limit the minimum values of destination.x & destination.y
+        destination.x = Mathf.Max(minXY.x, destination.x);
+        destination.y = Mathf.Max(minXY.y, destination.y);
+        // Interpolate from the current Camera position toward destination
+        destination = Vector3.Lerp(transform.position, destination, easing);
         // Force destination.z to be camZ to keep the camera far enough away
         destination.z = camZ;
         // Set the camera to the destination

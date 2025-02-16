e83f2bd2c7f6c9d38d6c096cc3278add4d972e6e
bcook03 <bnc121203@outlook.com>
2025-02-16 12:54:06 -0600

    Added projectile script

---
+++ Assets/Script/Projectile.cs
@@ -0,0 +1,57 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Projectile : MonoBehaviour
+{
+    const int LOOKBACK_COUNT = 10;
+
+    [SerializeField]
+    private bool _awake = true;
+    public bool awake{
+        get { return _awake; }
+        private set { _awake = value; }
+    }
+    
+    private Vector3 prevpos;
+    // This private Lsit stores the history of Projectile's move distance
+    private List<float> deltas = new List<float>();
+    private Rigidbody rigid;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody>();
+        awake = true;
+        prevpos = new Vector3(1000,1000,0);
+        deltas.Add(1000);
+    }
+
+    void FixedUpdate(){
+        if (rigid.isKinematic || !awake ) return;
+
+        Vector3 deltaV3 = transform.position - prevpos;
+        deltas.Add( deltaV3.magnitude );
+        prevpos = transform.position;
+
+        while (deltas.Count > LOOKBACK_COUNT) {
+            deltas.RemoveAt(0);
+        }
+
+        // Iterate over deltas and find the greatest one
+        float maxDelta = 0;
+
+        foreach (float f in deltas) {
+            if (f > maxDelta) maxDelta = f;
+        }
+
+        // If the Projectile hasn't moved more than the sleepThreshold
+        if (maxDelta <= Physics.sleepThreshold) {
+            // set awake to false and put the Rigidbody to sleep
+            awake = false;
+            rigid.Sleep();
+        }
+    }
+
+   
+}
---
+++ Assets/Script/RigidBodySleep.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class RigidBodySleep : MonoBehaviour
+{
+    private int sleepCountdown = 4;
+    private Rigidbody rigid;
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        if (sleepCountdown > 0 ) {
+            rigid.Sleep();
+            sleepCountdown--;
+        }
+    }
+}

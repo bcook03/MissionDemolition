9f8ea56a37281d38099e706380566e34e4bef5d0
bcook03 <bnc121203@outlook.com>
2025-02-20 17:55:07 -0600

    updated projectile and slingshot

--- Assets/Script/Projectile.cs
+++ Assets/Script/Projectile.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Projectile : MonoBehaviour
 {
     const int LOOKBACK_COUNT = 10;
+    static List<Projectile> PROJECTILES = new List<Projectile>();
 
     [SerializeField]
     private bool _awake = true;
@@ -25,6 +26,8 @@ public class Projectile : MonoBehaviour
         awake = true;
         prevpos = new Vector3(1000,1000,0);
         deltas.Add(1000);
+
+        PROJECTILES.Add( this );
     }
 
     void FixedUpdate(){
@@ -50,6 +53,15 @@ public class Projectile : MonoBehaviour
             // set awake to false and put the Rigidbody to sleep
             awake = false;
             rigid.Sleep();
+        }
+    }
+
+    private void OnDestroy() {
+        PROJECTILES.Remove(this);
+    }
+    static public void DESTROY_PROJECTILES() {
+        foreach(Projectile p in PROJECTILES) {
+            Destroy(p.gameObject);
         }
     }
 
